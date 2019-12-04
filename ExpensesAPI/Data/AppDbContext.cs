ae7eed1f5f2e67480f2f4cf193ce948c27b66f1e
Dean Agan <agandfr@gmail.com>
2019-12-04 11:30:20 +1100

    Initial backend code

---
+++ ExpensesAPI/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using ExpensesAPI.Models;
+
+namespace ExpensesAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("name=ExpensesDb")
+        {
+                
+        }
+        public DbSet<Entry> Entries { get; set; }
+    }
+}
