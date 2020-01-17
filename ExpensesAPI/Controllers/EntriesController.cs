6e34253de948a4af2f4b903bc271b7969b39085c
Dean Agan <agandfr@gmail.com>
2020-01-17 11:40:36 +1100

    Add HttpPost and updated connection string

--- ExpensesAPI/Controllers/EntriesController.cs
+++ ExpensesAPI/Controllers/EntriesController.cs
@@ -7,12 +7,14 @@ using System.Web.Http;
 using System.Web.Http.Results;
 using ExpensesAPI.Data;
 using System.Web.Http.Cors;
+using ExpensesAPI.Models;
 
 namespace ExpensesAPI.Controllers
 {
     [EnableCors("http://localhost:4200", "**", "*")]
     public class EntriesController : ApiController
     {
+        [HttpGet]
         public IHttpActionResult GetEntries()
         {
             try
@@ -22,6 +24,31 @@ namespace ExpensesAPI.Controllers
                     var entries = context.Entries.ToList();
 
                     return Ok(entries);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult PostEntry([FromBody] Entry entry)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                using (var context = new AppDbContext())
+                {
+                    context.Entries.Add(entry);
+                    context.SaveChanges();
+
+                    return Ok("Entry was created");
                 }
             }
             catch (Exception ex)
